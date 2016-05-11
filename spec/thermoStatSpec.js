b40099618b5811215009d01c7188a96ac25ca7a0
letianw <tmac4ever0824@gmail.com>
2016-05-11 21:51:06 +1000

    challenge 5-5 completed

--- spec/thermoStatSpec.js
+++ spec/thermoStatSpec.js
@@ -9,7 +9,7 @@ describe("Thermostat" ,function(){
 	it('returns default temperature', function(){
 		expect(thermostat.temperature).toEqual(20);
 	});
-  
+
   it('increase the temperature', function(){
   	thermostat.incrTemp();
   	expect(thermostat.temperature).toEqual(21);
@@ -19,4 +19,18 @@ describe("Thermostat" ,function(){
   	thermostat.decrTemp();
   	expect(thermostat.temperature).toEqual(19);
   });
+
+  it('has a minimum temperature of 10', function(){
+    thermostat.temperature = 10
+    expect(function(){
+      thermostat.decrTemp()
+    }).toThrowError('lowest temperature is 10!');
+  });
+
+ it('has a maximum temperature of 25 when powersave mode is on', function(){
+    thermostat.temperature = 25;
+    expect(function(){
+      thermostat.incrTemp()
+    }).toThrowError('Highest temperature is 25 in powersave mode!');
+ });
 });
--- src/thermoStat.js
+++ src/thermoStat.js
@@ -1,11 +1,18 @@
 function Thermostat(){
 	this.temperature = 20;
+  this.powerSave = true;
 };
 
 Thermostat.prototype.incrTemp = function() {
+  if (this.powerSave && this.temperature == 25) {
+    throw new Error('Highest temperature is 25 in powersave mode!');
+  }
 	this.temperature++;
 };
 
 Thermostat.prototype.decrTemp = function() {
+  if (this.temperature === 10) {
+    throw new Error('lowest temperature is 10!');
+  }
 	this.temperature--;
 };

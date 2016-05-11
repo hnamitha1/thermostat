f1312680e38b249bddc08978488a23ef07b2aba4
hnamitha1 <namitha.h@gmail.com>
2016-05-11 12:38:27 +0100

    requirment 2 completed

--- spec/thermoStatSpec.js
+++ spec/thermoStatSpec.js
@@ -9,5 +9,9 @@ describe("Thermostat" ,function(){
 	it('returns default temperature', function(){
 		expect(thermostat.temperature).toEqual(20);
 	});
-
+  
+  it('increase the temperature', function(){
+  	thermostat.incrTemp();
+  	expect(thermostat.temperature).toEqual(21);
+  });
 });

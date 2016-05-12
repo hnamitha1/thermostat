1f3efa1e7b5dff2dd05c8d83b02dfa36f91ff91a
Max Provin <maxprovin@outlook.com>
2016-05-12 13:40:31 +0100

    starting to implement weather api

--- js/interface.js
+++ js/interface.js
@@ -32,4 +32,12 @@ $(document).ready(function() {
     $('#temp').text(thermostat.getTemperature());
     $('#temp').attr('class', thermostat.energyUsage());
   }
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+        $('#outside-temp').text(data.main.temp);
+    });
+  });
+
 });
